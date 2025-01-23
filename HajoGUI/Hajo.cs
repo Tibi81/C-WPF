bb9a7a7a6a4207516c9fc65dfb84213e42d203ef
Tibi81 <kisss.tibi@gmail.com>
2025-01-23 22:03:14 +0100

    Add project files.

---
+++ HajoGUI/Hajo.cs
@@ -0,0 +1,20 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HajoGUI
+{
+    internal class Hajo
+    {
+        public string Nev { get; set; }
+        public string Tipus { get; set; }
+        public int Ev { get; set; }
+        public Hajo(MySqlDataReader reader)
+        {
+            Nev = reader.GetString(0);
+            Tipus = reader.GetString(1);
+            Ev = reader.GetInt32(2);
+        }
+    }
+}
---
+++ HajoGUI/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HajoGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Hajo> adatok = new List<Hajo>();
+        private readonly string connects = "Server=localhost; Database=hajok1;Uid=root;password='';";
+        private readonly MySqlConnection conn;
+        public MainWindow()
+        {
+            InitializeComponent();
+            conn = new MySqlConnection(connects);
+            conn.Open();
+            string lekerdezes1 = "SELECT nev, tipus, ev FROM hajo;";
+            MySqlCommand cmd = new MySqlCommand(lekerdezes1, conn);
+            MySqlDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                Hajo x = new Hajo(dr);
+                adatok.Add(x);
+            }
+            dr.Close();
+            Dtg1.ItemsSource = adatok;  
+        }
+       
+
+        private void Btn1_Click(object sender, RoutedEventArgs e)
+        {
+            int index = Dtg1.SelectedIndex + 1;
+            string lekerdezes2 = $"SELECT tulajdonos.tnev FROM hajo, tulajdonos WHERE hajo.tid = tulajdonos.id AND hajo.id = {index};";
+            MySqlCommand cmd = new MySqlCommand(lekerdezes2, conn);
+            MySqlDataReader dr = cmd.ExecuteReader();
+            dr.Read();
+            txt1.Text = dr.GetString(0);
+            dr.Close();
+        }
+
+        private void Btn2_Click(object sender, RoutedEventArgs e)
+        {
+            string lekerdezes2 = "SELECT COUNT(*) FROM hajo WHERE uzemel = 1;";
+            MySqlCommand cmd = new MySqlCommand(lekerdezes2, conn);
+            MySqlDataReader dr = cmd.ExecuteReader();
+            dr.Read();
+            txt2.Text = dr.GetInt32(0).ToString();
+            dr.Close();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int index = Dtg1.SelectedIndex + 1;
+            string lekerdezes2 = $"SELECT tipus FROM hajo, tulajdonos WHERE hajo.tid = tulajdonos.id AND hajo.id = {index};";
+            MySqlCommand cmd = new MySqlCommand(lekerdezes2, conn);
+            MySqlDataReader dr = cmd.ExecuteReader();
+            dr.Read();
+            lbl1.Content = dr.GetString(0);
+            dr.Close();
+        }
+    }
+}
